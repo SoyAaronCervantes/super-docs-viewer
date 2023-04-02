fa9985ddc39f2b3ffebbdfae165469929375ca62
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2023-04-02 05:43:05 -0600

    Squashed commit of the following:
    commit b1c227c4a8ebdaf203e283215ae2a5f2985dca81
    Author: Aaron Cervantes <soy.aaron.cervantes@gmail.com>
    Date:   Sun Apr 2 05:42:39 2023 -0600
    
        feat: Integrated @angular/fire to project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'doc-viewer-1bfd3',
+    appId: '1:44409254949:web:d50c42fe940755eb24bbcc',
+    databaseURL: 'https://doc-viewer-1bfd3-default-rtdb.firebaseio.com',
+    storageBucket: 'doc-viewer-1bfd3.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDro91guf-QMtRNfL36s_s5R8arA0RF6sY',
+    authDomain: 'doc-viewer-1bfd3.firebaseapp.com',
+    messagingSenderId: '44409254949',
+    measurementId: 'G-HYX15PDQGW',
+  },
   production: true
 };
