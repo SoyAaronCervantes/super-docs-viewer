6bebbe94bb928ebcd179748c06d35a2864bde479
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2023-04-03 22:29:38 -0600

    Squashed commit of the following:
    commit 101428363b6c764a2c1c99ca9ac28fd689d62f42
    Author: Aaron Cervantes <soy.aaron.cervantes@gmail.com>
    Date:   Mon Apr 3 22:29:14 2023 -0600
    
        fix: get right photo with document id

--- src/app/pages/document/document.component.ts
+++ src/app/pages/document/document.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {DocumentsFirestoreService} from "../../services/firebase/documents/documents-firestore.service";
 import {AnnotationsFirestoreService} from "../../services/firebase/annotations/annotations-firestore.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-document',
@@ -10,12 +11,18 @@ import {AnnotationsFirestoreService} from "../../services/firebase/annotations/a
 export class DocumentComponent {
   size = 100;
 
-  document$ = this.documentsFirestoreService.document$('1');
-  annotations$ = this.annotationsFirestoreService.getAnnotationsFromDocument('1');
+  document$ = this.documentsFirestoreService.document$(this.getDocumentIdFromUrl());
+  annotations$ = this.annotationsFirestoreService.getAnnotationsFromDocument(this.getDocumentIdFromUrl());
 
   constructor(
     private documentsFirestoreService: DocumentsFirestoreService,
-    private annotationsFirestoreService: AnnotationsFirestoreService
+    private annotationsFirestoreService: AnnotationsFirestoreService,
+    private activatedRoute: ActivatedRoute,
   ) {}
+
+  private getDocumentIdFromUrl() {
+    const paramMap = this.activatedRoute.snapshot.paramMap;
+    return paramMap.get('id')!!;
+  }
 
 }
