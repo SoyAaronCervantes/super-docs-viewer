0e649cce2fc3b14320c341210c7e48203fb2884f
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2023-04-09 19:20:03 -0600

    Squashed commit of the following:
    commit 59c90864358965d54a7c354675c3e0a747fdf946
    Author: Aaron Cervantes <soy.aaron.cervantes@gmail.com>
    Date:   Sun Apr 9 19:13:18 2023 -0600
    
        feat: Scale coords based on resize percetange
    
    commit a1e1f38cc7f6c0c9c16df8bb5845f7febf9a1040
    Author: Aaron Cervantes <soy.aaron.cervantes@gmail.com>
    Date:   Sun Apr 9 18:53:57 2023 -0600
    
        feat: Update functionality to get coords

--- src/app/components/coordinate/coordinate-container/coordinate-container.component.ts
+++ src/app/components/coordinate/coordinate-container/coordinate-container.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {MatSidenav} from "@angular/material/sidenav";
 
 import {ImagesStorageService} from "../../../services/firebase/images/images-storage.service";
@@ -7,6 +7,7 @@ import {DocumentInterface} from "../../../interfaces/document.interface";
 import {Annotation} from "../../../interfaces/annotation.interface";
 
 import {EMPTY, Observable} from "rxjs";
+import {Point} from "@angular/cdk/drag-drop";
 
 @Component({
   selector: 'app-coordinate-container',
@@ -18,6 +19,8 @@ export class CoordinateContainerComponent {
   @Input() annotations: Annotation[];
   @Input() sidenav: MatSidenav;
   @Input() size: number;
+  @ViewChild('annotationContainer')
+  annotationContainer: ElementRef<HTMLDivElement>;
   url$: Observable<string> = EMPTY;
 
   constructor(
@@ -29,4 +32,12 @@ export class CoordinateContainerComponent {
     if (this.url$ !== EMPTY) { return; }
     this.url$ = this.imagesStorageService.getImageUrl(annotation.image);
   }
+
+  getAnnotationsPosition(point: Point, percentageSize: number): Point {
+    // Get the position of the annotation from the percentage of the image
+    const x = Math.round((point.x * percentageSize) / 100);
+    const y = Math.round((point.y * percentageSize) / 100);
+    return {x, y};
+  }
+
 }
